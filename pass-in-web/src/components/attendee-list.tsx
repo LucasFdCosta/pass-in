87d37e31d359181731d57e280b81a505825783ec
Lucas Costa <lucas.co090869@gmail.com>
2024-04-05 16:03:35 -0300

    create bogus data with fakerjs + create logic to change pages + handle dates with dayjs

--- pass-in-web/src/components/attendee-list.tsx
+++ pass-in-web/src/components/attendee-list.tsx
@@ -4,8 +4,39 @@ import { Table } from "./table/table";
 import { TableHeader } from "./table/table-header";
 import { TableCell } from "./table/table-cell";
 import { TableRow } from "./table/TableRow";
+import { ChangeEvent, useState } from "react";
+import { attendees } from "../data/attendees";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime)
 
 export function AttendeeList() {
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+
+  const totalPages = Math.ceil(attendees.length / 10);
+
+  function goToNextPage() {
+    setPage(page + 1);
+  }
+
+  function goToPreviousPage() {
+    setPage(page - 1);
+  }
+
+  function goToFirstPage() {
+    setPage(1);
+  }
+
+  function goToLastPage() {
+    setPage(totalPages);
+  }
+
+  function onSearchInputChanged(e: ChangeEvent<HTMLInputElement>) {
+    setSearch(e.target.value)
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -15,7 +46,7 @@ export function AttendeeList() {
 
         <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
-          <input className="bg-transparent h-auto border-0 p-0 text-sm ring-0 flex-1 outline-none" placeholder="Search attendee..." />
+          <input onChange={onSearchInputChanged} className="bg-transparent h-auto border-0 p-0 text-sm ring-0 flex-1 outline-none" placeholder="Search attendee..." />
         </div>
       </div>
 
@@ -33,21 +64,21 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 8 }).map((_, index) => {
+          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
             return (
-              <TableRow key={index}>
+              <TableRow key={attendee.id}>
                 <TableCell className="py-3 px-4 text-sm text-zinc-300">
                   <input className="size-4 bg-black/20 rounded border border-white/10" type="checkbox" />
                 </TableCell>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">12313</TableCell>
+                <TableCell className="py-3 px-4 text-sm text-zinc-300">{attendee.id}</TableCell>
                 <TableCell className="py-3 px-4 text-sm text-zinc-300">
                   <div className="flex flex-col gap-1">
-                    <span className="font-semibold text-white">Lucas Costa</span>
-                    <span>lucas@email.com</span>
+                    <span className="font-semibold text-white">{attendee.name}</span>
+                    <span>{attendee.email}</span>
                   </div>
                 </TableCell>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">7 days ago</TableCell>
-                <TableCell className="py-3 px-4 text-sm text-zinc-300">3 days ago</TableCell>
+                <TableCell className="py-3 px-4 text-sm text-zinc-300">{dayjs().to(attendee.createdAt)}</TableCell>
+                <TableCell className="py-3 px-4 text-sm text-zinc-300">{dayjs().to(attendee.checkedInAt)}</TableCell>
                 <TableCell className="py-3 px-4 text-sm text-zinc-300">
                   <IconButton transparent>
                     <MoreHorizontal className="size-4" />
@@ -59,22 +90,22 @@ export function AttendeeList() {
         </tbody>
         <tfoot>
           <tr>
-            <TableCell colSpan={3}>Showing 10 out of 228 items</TableCell>
+            <TableCell colSpan={3}>Showing 10 out of {attendees.length} items</TableCell>
             <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
-                <span>Page 1 of 23</span>
+                <span>Page {page} of {totalPages}</span>
 
                 <div className="flex gap-1.5">
-                  <IconButton>
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
                     <ChevronsLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
