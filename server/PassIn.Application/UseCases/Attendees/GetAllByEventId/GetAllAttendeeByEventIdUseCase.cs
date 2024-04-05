4a8ce2da22cef2cc9569f2bc8cc2a18c5891b8b5
Lucas Costa <lucas.co090869@gmail.com>
2024-04-04 21:04:44 -0300

    create attendee controller + endpoints to register and get all attendees from an event

---
+++ server/PassIn.Application/UseCases/Attendees/GetAllByEventId/GetAllAttendeeByEventIdUseCase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+
+namespace PassIn.Application.UseCases.Attendees.GetAllByEventId
+{
+    public class GetAllAttendeeByEventIdUseCase
+    {
+        private readonly PassInDbContext _dbContext;
+
+        public GetAllAttendeeByEventIdUseCase()
+        {
+            _dbContext = new PassInDbContext();
+        }
+
+        public ResponseAllAttendeesJson Execute(Guid eventId)
+        {
+            var entity = _dbContext.Events.Include(ev => ev.Attendees).FirstOrDefault(ev => ev.Id == eventId);
+
+            if (entity is null) throw new NotFoundException("Event not found");
+
+            return new ResponseAllAttendeesJson
+            {
+                Attendees = entity.Attendees.Select(at => new ResponseAttendeeJson
+                {
+                    Id = at.Id,
+                    Name = at.Name,
+                    Email = at.Email,
+                    CreatedAt = at.Created_At
+                }).ToList()
+            };
+        }
+    }
+}
